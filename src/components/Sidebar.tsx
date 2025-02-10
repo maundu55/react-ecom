ca26489c4e2b4253bc91be12d8fe94d25df0c4d0
maundu55 <marymaundu.mm@gmail.com>
2025-02-10 11:39:20 +0300

    react ecommerce project

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const Sidebar = () => {
+    const [categories, setCategories] useState<string[]>([]);
+
+    
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
