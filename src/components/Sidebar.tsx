ba0f9b921bf80414b12fcd451d09d02ec92c9b01
maundu55 <marymaundu.mm@gmail.com>
2025-02-10 14:06:35 +0300

    install tailwind css

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -49,6 +49,35 @@ const Sidebar = () => {
                 <input type="text" className="border-2 mr-2 px-5 py-3 mb-3 w-full" placeholder="Min" />
                 <input type="text" className="border-2 mr-2 px-5 py-3 mb-3 w-full" placeholder="Max" />
             </div>
+
+        {/* Categories Section */}
+        <section>
+        <div className="mb-5">
+            <h2 className="text-xl font-semibold mb-3">Categories</h2>
+        </div>
+            {categories.map((category, index)=>(
+                <label key={index} className="block mb-2">
+                    <input type="radio" name="category" value={category} className="mr-2 w-[16px] h-[16px]"/>
+                    {category.toUpperCase()}
+                </label>
+               
+            ))}
+            </section>
+
+            {/* Keywords Section */}
+            <div className="mb-5 mt-4">
+                <h2 className="text-xl font-semibold mb-3">Keywords</h2>
+                <div>
+                    {keywords.map((keyword, index)=>(
+                        <button key={index} className="block mb-2 px-4 py-2 w-full text-left border rounded hover:bg-gray-200">
+                            {keyword.toUpperCase()}
+                        </button>
+                    ))}
+                </div>
+            </div>
+            <button className="w-full mb-[4rem] py-2 bg-black text-white rounded mt-5">
+                Reset Filter
+            </button>
         </section>
     </div>
   )
